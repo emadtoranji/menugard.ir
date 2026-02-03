7d2d016d3a37ffd82006cc7d0aa97a6529fda784
Emad <emadtoranji6@gmail.com>
2026-02-03 12:27:08 +0330

    add StoreExplorerSource

--- src/lib/prismaEnums.js
+++ src/lib/prismaEnums.js
@@ -1,9 +1,11 @@
 import {
   StoreCategoriesKey,
   StoreItemsCategoriesKey,
+  StoreExplorerSource,
   ValidCurrency,
 } from '../generated/client';
 
 export const storeCategories = Object.values(StoreCategoriesKey);
 export const storeCurrencies = Object.values(ValidCurrency);
 export const storeItemsCategoriesKey = Object.values(StoreItemsCategoriesKey);
+export const storeExplorerSource = Object.values(StoreExplorerSource);
