1be0cdef1244eb9cbaca428435c6330e4fed8fe3
Emad <emadtoranji6@gmail.com>
2026-02-03 16:52:15 +0330

    improve

--- src/app/[lng]/(store)/(one-store)/store/[slug]/page.jsx
+++ src/app/[lng]/(store)/(one-store)/store/[slug]/page.jsx
@@ -1,43 +1,40 @@
 import StoreNotFound from '../../../StoreNotFound';
 import { getStores } from '@server/getStores';
 import { getT } from '@i18n/server';
-import { cache } from 'react';
 import StoreComponent from './StoreComponent';
+import { headers } from 'next/headers';
+import LogCookie from './LogCookie';
+import { storeExplorerSource } from '@/src/lib/prismaEnums';
 
-export async function generateMetadata(props) {
-  const m = await import('@utils/metadata');
-  return m.generateMetadata(props, { forcedPage: 'storeSlug' });
-}
-
-const fetchStoreCached = cache(async ({ slug }) => {
-  const response = await getStores({ slug });
-
-  if (!response?.ok || !response?.result?.slug) {
-    return {};
-  }
-
-  return {
-    store: response.result,
-  };
-});
-
-export default async function Page({ params }) {
+export default async function Page({ params, searchParams }) {
   const { lng, slug = undefined } = (await params) || {};
   const { t } = await getT(lng, 'store');
+  const useSearchParams = await searchParams;
+  const tableId = useSearchParams?.table ?? useSearchParams?.utm_table ?? null;
+  const source =
+    useSearchParams?.source ?? useSearchParams?.utm_source ?? 'direct';
 
-  if (!slug) {
-    return <StoreNotFound t={t} />;
-  }
+  if (!slug) return <StoreNotFound t={t} />;
 
-  const data = await fetchStoreCached({ slug });
-  const { store = {} } = data;
+  const response = await getStores({ slug });
+  const store = response?.result ?? {};
 
-  if (!store?.slug) {
-    return <StoreNotFound t={t} />;
-  }
+  if (!store?.slug) return <StoreNotFound t={t} />;
+  const headersList = await headers();
 
   return (
     <section className='container-fluid'>
+      <LogCookie
+        storeId={store.id}
+        tableId={tableId}
+        source={storeExplorerSource.includes(source) ? source : 'unknown'}
+        ipAddress={
+          headersList.get('cf-connecting-ip') ||
+          headersList.get('x-forwarded-for')?.split(',')[0] ||
+          headersList.get('x-real-ip') ||
+          null
+        }
+      />
       <StoreComponent store={store} lng={lng} />
     </section>
   );
