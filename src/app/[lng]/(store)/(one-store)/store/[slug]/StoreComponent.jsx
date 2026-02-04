05b24badf378b52077c36796936fd76f7606a9ea
Emad <emadtoranji6@gmail.com>
2026-02-04 14:10:28 +0330

    add key to ItemContent

--- src/app/[lng]/(store)/(one-store)/store/[slug]/StoreComponent.jsx
+++ src/app/[lng]/(store)/(one-store)/store/[slug]/StoreComponent.jsx
@@ -25,6 +25,7 @@ export default function StoreComponent({ store, lng }) {
       />
 
       <ItemContent
+        key={activeCategory}
         storeCurrency={store.currency}
         items={Object.values(
           store.items
