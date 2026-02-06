88761e219ba98cccba1054dca4e8d3c8f94bc22e
Emad <emadtoranji6@gmail.com>
2026-02-06 21:38:50 +0330

    improve UI

--- src/components/Loading/Index.jsx
+++ src/components/Loading/Index.jsx
@@ -3,7 +3,7 @@ import './Style.css';
 export default function Loading() {
   return (
     <div
-      className={`loading-screen position-fixed top-0 left-0 w-100 h-100 d-flex justify-content-center align-items-center`}
+      className={`loader-section position-fixed top-0 start-0 d-flex justify-content-center align-items-center`}
     >
       <div className={`loader`}></div>
     </div>
