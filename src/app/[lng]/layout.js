db26f9e39f409aa9a219a4fcc861707863b7b374
Emad <emadtoranji6@gmail.com>
2026-02-12 15:03:58 +0330

    remove imporing multiple css file and add packages instead of CDN

--- src/app/[lng]/layout.js
+++ src/app/[lng]/layout.js
@@ -1,12 +1,11 @@
 import '@styles/general/globals.css';
-import '@styles/general/fonts.css';
-import '@styles/general/colors.css';
+import 'animate.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { Gulzar, Roboto, Vazirmatn, Share_Tech } from 'next/font/google';
+import { Roboto, Vazirmatn, Share_Tech } from 'next/font/google';
 import { fallbackLng, languages } from '@i18n/settings';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
-import Script from 'next/script';
 
 import localFont from 'next/font/local';
 
@@ -55,37 +54,17 @@ export default async function RootLayout({ children, params }) {
     <html
       lang={lng}
       dir={isRTL ? 'rtl' : 'ltr'}
-      className={
+      className={`w-full box-border overflow-x-hidden m-0 p-0 ${
         isRTL
           ? `${vazirmatn.variable} ${asiatech.variable}`
           : `${roboto.variable} ${shareTech.variable}`
-      }
+      }`}
     >
-      <head>
-        <link
-          rel='stylesheet'
-          href='https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css'
-        />
-        <link
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css'
-          integrity='sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB'
-          crossOrigin='anonymous'
-          rel='stylesheet'
-        />
-        <link
-          rel='stylesheet'
-          href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css'
-        ></link>
-      </head>
-
-      <body className={`container-fluid d-flex flex-column min-vh-100 m-0 p-0`}>
+      <body
+        className={`flex flex-col min-h-screen w-full box-border overflow-x-hidden m-0 p-0`}
+      >
         <Toaster />
         <SessionProvider>{children}</SessionProvider>
-        <Script
-          src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js'
-          integrity='sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI'
-          crossOrigin='anonymous'
-        ></Script>
       </body>
     </html>
   );
