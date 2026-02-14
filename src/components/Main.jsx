73cac4a0e94ac2cd138b7bdd1457415e7e016e65
Emad <emadtoranji6@gmail.com>
2026-02-14 21:53:49 +0330

    add hasMinHFull

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
-export default function Main({ children, customClass }) {
+export default function Main({ children, customClass, hasMinHFull = true }) {
   return (
     <main
-      className={`w-full min-h-full flex-1 ${customClass ? customClass : 'pt-5 mb-5 mx-0 px-0'}`}
+      className={`w-full ${hasMinHFull ? 'min-h-full' : ''} flex-1 ${customClass ? customClass : 'pt-5 mb-5 mx-0 px-0'}`}
     >
       {children}
     </main>
